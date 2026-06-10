950cffd9839b9db71b602a2afc5f10b4a6ba6c7a
gustavnerisfx <gustavonerissilva0@gmail.com>
2026-06-10 09:47:19 -0300

    .

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Drawer = createDrawerNavigator();
+const gay = createDrawerNavigator();
 
 function DrawerNavigation() {
   return (

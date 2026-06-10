2f89e4899ea6160fa09cd1599916885cc393e565
gustavnerisfx <gustavonerissilva0@gmail.com>
2026-06-10 11:30:29 -0300

    .

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { BlurView } from 'expo-blur';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from 'components/HomeScreen';
 
-const gay = createDrawerNavigator();
+// icones do react icons
+import { TbCards } from "react-icons/tb";
+import { PiSoccerBallFill } from "react-icons/pi";
+import { GiBrazilFlag } from "react-icons/gi";
 
-function DrawerNavigation() {
+const Tab = createBottomTabNavigator();
+
+function TabNavigation() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
-    </Drawer.Navigator>
+    <Tab.Navigator
+      ScreenOptions={{
+        tabBarStyle: { position: 'absolute' },
+        tabBarBackground: () => (
+          <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
+        ),
+
+        
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Cards" component={CardsScreen} />
+      <Tab.Screen name="Matches" component={MatchesScreen} />
+      <Tab.Screen name="Flag" component={FlagScreen} />
+    </Tab.Navigator>
   );
 }
 
 export default function App() {
   return (
     <View>
-      <DrawerNavigation />
+      <TabNavigation /> 
     </View>
   );
 }

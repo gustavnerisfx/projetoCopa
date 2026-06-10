222d526caf1148229c84619c93ccdf9a4c4fcfb4
gustavnerisfx <gustavonerissilva0@gmail.com>
2026-06-10 09:44:45 -0300

    .

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigation() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <View>
+      <DrawerNavigation />
+    </View>
+  );
+}
